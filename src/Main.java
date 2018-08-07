2eeb008926bc69d8ce06fe527a97510627fdcb33
hhive <31347477+hhive@users.noreply.github.com>
2018-08-08 00:10:40 +0800

    test_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+
+
 public class Main {
     public static void main(String[] arg){
+        System.out.println();
+        test2 a = new test2(String.valueOf("1946"));
+        System.out.println(a.isBornBoomer());
+        System.out.println(String.valueOf("1946"));
+    }
+}
 
+class test2 {
+    private String birthString;
+    private static String startString, endString;
+
+    static {
+
+        startString = String.valueOf("1946");
+        endString = String.valueOf("1964");
+    }
+
+    public test2(String birthString) {
+        this.birthString = birthString;
+    }
+
+    boolean isBornBoomer() {
+        return birthString.compareTo(startString) >= 0 && birthString.compareTo(endString) < 0;
+    }
+
+    public static void main(String[] args)
+    {
+        test2 a= new test2(String.valueOf("1955"));
+        System.out.println(a.isBornBoomer());
     }
 }
