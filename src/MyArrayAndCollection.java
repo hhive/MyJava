5f4462ca590b433f927ac62d2353ff4e69808397
hhive <31347477+hhive@users.noreply.github.com>
2018-08-19 01:07:31 +0800

    Test_13

---
+++ src/MyArrayAndCollection.java
@@ -0,0 +1,556 @@
+
+
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * Main
+ */
+public class MyArrayAndCollection {
+
+    public static void main(String[] arg) {
+
+        MyMap myMap = new MyMap();
+        myMap.printForHashMap();
+//        MyQueue myQueue = new MyQueue();
+//        myQueue.printForQueue();
+//        MyList myList = new MyList();
+//        myList.printForList();
+
+//        ParentClass a = new ChildClass(4, 5, 6);
+//        ChildClass b = new ChildClass(1, 2, 3);
+//        ((ChildClass) a).childFunction(); //true
+//        a.childFunction(); //error
+//        b.parentFunction(); //true
+
+//        new MySet().printForTreeSet();
+//        LambdaQs lambdaQs = new LambdaQs();
+//        lambdaQs.PrintForLam(lambdaQs);
+
+//        MyCollection myCollection = new MyCollection();
+//        myCollection.printForCollection();
+//        myCollection.printForStream();
+        //myCollection.printForIterator();
+
+//        new splitNumAndLetter().mySplit();
+
+//        new MyArray().myprint();
+
+    }
+}
+
+/**
+ * Operational tests on Map
+ * Implement class: HashMap, Hashtable(Properties), LinkedHashMap,
+ * SortedMap, TreeMap, weakHashMap, IdentityHashMap, EnumMap
+ */
+class MyMap {
+    private Map map = new HashMap();
+    /**
+     * print
+     */
+    public void printForHashMap() {
+        map.put("Java", 103);
+        map.put("Java EE", 10);
+        map.put("AJAX", 79);
+        map.put("Android", 79);
+        System.out.println(map.put("Java EE", 99));
+        System.out.println(map);
+        System.out.println("is include Java?" + map.containsKey("Java"));
+        System.out.println("is include 99?" + map.containsValue(99));
+        for (Object key : map.keySet()) {
+            System.out.println(key + "-->" + map.get(key));
+        }
+        map.remove("AJAX");
+        map.merge("Java", 10, (oldVal, param) -> (Integer) oldVal + (Integer) param);
+        System.out.println(map);
+        map.computeIfPresent("Java", (key, value) -> ((String) key).length());
+        System.out.println(map);
+    }
+}
+
+/**
+ * Operational tests on Queue
+ * The queue does not allow random access to element int the queue
+ * but ArrayDeque could;
+ * Implement class: PriorityQueue, ArrayDeque
+ */
+class MyQueue {
+    //use Arrayeque as stack
+    private ArrayDeque stack = new ArrayDeque();
+
+    private ArrayDeque queue = new ArrayDeque();
+
+    /**
+     * print the test of ArrayDeque
+     */
+    public void printForStack() {
+        stack.push("Java");
+        stack.push("Java EE");
+        stack.push("Android");
+        System.out.println(stack);
+        System.out.println(stack.peek() + ",do not delete");
+        System.out.println(stack.pop() + ",delete");
+        System.out.println(stack);
+    }
+    /**
+     * print the test of ArrayDeque
+     */
+    public void printForQueue() {
+        queue.offer("Java");
+        queue.offer("Java EE");
+        queue.offer("Android");
+        System.out.println(queue);
+        System.out.println(queue.peek() + ",do not delete");
+        System.out.println(queue.pop() + ",delete");
+        System.out.println(queue);
+    }
+}
+
+/**
+ * The test of List
+ * Implement class: ArrayLis, Vector, LinkedList
+ */
+class MyList {
+    //Use initialCapacity to specify the length
+    private List books = new ArrayList();
+
+    /**
+     *Operational tests on List
+     */
+    public void printForList() {
+        //ArrayList use initialCapacity to specify the length
+        ArrayList a = new ArrayList(12);
+        //add capacity
+        a.ensureCapacity(10);
+
+        books.add(new String("Java"));
+        books.add("Android");
+        books.add(new String("Java EE"));
+        System.out.println(books);
+        //Insert a new String into the second position
+        books.add(1, new String("AJAX"));
+
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println(books.get(i));
+        }
+        books.remove(2);
+        System.out.println(books);
+        //Determine the position of the specified element in the List collection,
+        // print "1",indicates that it is in second place
+        //Note: it uses "new"
+        System.out.println(books.indexOf(new String("AJAX")));
+        books.set(1, new String("Java"));
+        //Intercept the first element(include) of the collection
+        // to the second element(not included) into a subset
+        System.out.println(books.subList(1, 2));
+
+//        String s = new String("s");
+//        String s1 = "s";
+//        System.out.println(s.equals(s1));
+
+        books.sort((o1, o2) -> ((String) o1).length() - ((String) o2).length());
+        System.out.println(books);
+        //Replace the element with the length of the element;
+        books.replaceAll(ele -> ((String) ele).length());
+        System.out.println(books);
+        //Comparing Iterator with listIterator,
+        // you can see that listIterator adds the function of forward iteration
+        //It needs to have elements in front of it
+        books.listIterator().hasPrevious();
+
+    }
+}
+
+
+/**
+ * Set is basically the same as Collection,
+ * but Set dose not allow duplicate elements;
+ *Implement class: HashSet, LinkedHashSet, TreeSet, EnumSet;
+ */
+class MySet {
+    /**
+     * Enum class
+     */
+    enum Season {
+        Spring, Summer, Fall, Winter
+    }
+    /**
+     * Custom Sort of TreeSet
+     */
+    class M {
+        private int age;
+        /**
+         *
+         */
+        M(int age) {
+            this.age = age;
+        }
+        /**
+         *@return implement "toString" to print M`s object,
+         * otherwise it will print the address of M`s object
+         */
+        public String toString() {
+            return "M[age:" + age + "]";
+        }
+    }
+    /**
+     * print the test of EnumSet
+     */
+    public void printForEnum() {
+        EnumSet es4 = EnumSet.range(Season.Summer, Season.Winter);
+        System.out.println(es4);
+        EnumSet es5 = EnumSet.complementOf(es4);
+        System.out.println(es5);
+    }
+    /**
+     * print the test of TreeSet
+     * It implements SortSet
+     */
+    public void printForTreeSet() {
+        //Custom Sort of TreeSet
+        TreeSet ts = new TreeSet((o1, o2) -> {
+            M m1 = (M) o1;
+            M m2 = (M) o2;
+            //Determine the size according to the "age" attribute
+            // of the M`s object,age bigger M`s object is smaller
+            return m1.age > m2.age ? -1 : m1.age < m2.age ? 1 : 0;
+        });
+        ts.add(new M(5));
+        ts.add(new M(-3));
+        ts.add(new M(9));
+        System.out.println(ts);
+    }
+}
+
+/**
+ * interface for LambdaQa test
+ */
+interface MyInterface {
+    /**
+     * test
+     */
+    interface Eatable {
+        /**
+         *  test
+         */
+        void taste();
+    }
+
+    /**
+     * test
+     */
+    interface Flyable {
+        /**
+         * @param weather test
+         */
+        void fly(String weather);
+    }
+
+    /**
+     * test
+     */
+    interface Addable {
+        /**
+         * @param a test
+         * @param b test
+         * @return  test
+         */
+        int add(int a, int b);
+    }
+
+    /**
+     * test
+     */
+    @FunctionalInterface
+    interface Converter {
+        /**
+         *
+         * @param from test
+         * @return test
+         */
+        Integer convert(String from);
+    }
+
+    /**
+     * test
+     */
+    @FunctionalInterface
+    interface MyTest {
+        /**
+         *
+         * @param a test
+         * @param b test
+         * @param c test
+         * @return test
+         */
+        String test(String a, int b, int c);
+    }
+
+    /**
+     *
+     */
+    @FunctionalInterface
+    interface YourTest {
+        /**
+         *
+         * @param title test
+         * @return test
+         */
+        JFrame win(String title);
+    }
+
+    /**
+     *
+     * @param e test
+     */
+}
+
+/**
+ *  The usage of LambdaQa
+ */
+class LambdaQs implements MyInterface {
+
+    /**
+     *
+     * @param e test
+     */
+    public void eat(Eatable e) {
+        //输出对象e
+        System.out.println(e);
+        e.taste();
+    }
+
+    /**
+     *
+     * @param f test
+     */
+    public void drive(Flyable f) {
+        System.out.println("我开：" + f);
+        f.fly("晴天");
+    }
+
+    /**
+     *
+     * @param add test
+     */
+    public void toAdd(Addable add) {
+        System.out.println("5+3:" + add.add(5, 3));
+    }
+
+    /**
+     *
+     * @param lq test
+     */
+    public void printForLam(LambdaQs lq) {
+
+        //There is only one statement , you can omit the curly braces
+        lq.eat(() -> System.out.println("苹果味道好！"));
+        //There is only one parameter ,you can omit parenthesis
+        lq.drive(weather -> {
+            System.out.println("今天天气是：" + weather);
+            System.out.println("直升机飞行平稳");
+        });
+        //There is only one statement,
+        // so you can omit the "return" keyword even if the expression requires a return values
+        //why can I get his object by implementing its abstract method?
+        lq.toAdd((a, b) -> a + b);
+        //Cast an expression using a functional interface(Forced type conversion
+        Object obj = (Runnable) () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println();
+            }
+        };
+    }
+
+    /**
+     *
+     */
+    public void printForLamRefer() {
+        //Converter converter = from -> Integer.valueOf(from);
+        Converter converter1 = Integer::valueOf;
+        Integer val = converter1.convert("99");
+        //方法引用替代Lamdba表达式：引用类方法
+        //函数式接口中被实现方法的全部参数传给该类方法作为参数
+        System.out.println(val);
+
+        //Converter converter2 = from -> "fkit.org".indexOf(from);
+        Converter converter3 = "fkit.org"::indexOf;
+        //indexOf 方法返回一个整数值，指出 String 对象内子字符串的开始位置。如果没有找到子字符串，则返回-1。
+        Integer value = converter3.convert("it");
+        System.out.println(value);
+
+        //YourTest yt = (String a) -> new JFrame(a);
+        YourTest yt1 = JFrame::new;
+        JFrame jf = yt1.win("我的窗口");
+        System.out.println(jf);
+
+        //MyTest mt = (a, b, c) -> a.substring(b, c);
+        MyTest mt1 = String::substring;
+        String str = mt1.test("Java I Love you", 2, 9);
+        System.out.println(str);
+    }
+}
+
+/**
+ *
+ */
+class MyCollection {
+    private Collection c = new ArrayList();
+    private Collection books = new HashSet();
+    //Synchronous control
+    private Collection c1 = Collections.synchronizedCollection(new ArrayList());
+    private List list = Collections.synchronizedList(new ArrayList());
+    private Set s = Collections.synchronizedSet(new HashSet());
+    private Map m = Collections.synchronizedMap(new HashMap());
+
+
+
+    /**
+     *
+     */
+    public void printForCollection() {
+        c.add("孙悟空");
+        c.add(6);
+        System.out.println("c的元素数：" + c.size());
+        c.remove(6);
+        System.out.println("c中是否包含“孙悟空”字符串：" + c.contains("孙悟空"));
+        c.add("Java");
+        System.out.println("c集合的元素：" + c);
+
+        books.add("Java");
+        books.add("Android");
+        System.out.println("c集合是否完全包含books集合？" + c.containsAll(books));
+        //用c集合减去books集合里的元素
+        c.removeAll(books);
+        System.out.println("c中的元素：" + c);
+        c.clear();
+        System.out.println("c中的元素：" + c);
+        //控制books集合中只剩下c集合里也包含的元素
+        books.retainAll(c);
+        System.out.println("books集合的元素：" + books);
+        books.add("q");
+        books.add("a");
+        books.add("w");
+        for (Object obj : books) {
+            System.out.println(obj);
+        }
+    }
+    /**
+     * print usage for Iterator
+     */
+    public void printForIterator() {
+        books.add("q");
+        books.add("a");
+        books.add("w");
+        //使用Lambda表达式，调用forEach()方法遍历集合
+        books.forEach(obj -> System.out.println("迭代集合元素：" + obj));
+        Iterator it = books.iterator();
+        System.out.println(it.hasNext());
+        while (it.hasNext()) {
+            //it.next()返回的是Object类型
+            String book = (String) it.next();
+            System.out.println("book:" + book);
+            //使用Iterator迭代过程中，不可修改集合元素，类似books.remove(book)
+            if (book.equals("a")) {
+                it.remove();
+            }
+            book = "b";
+        }
+        System.out.println(books);
+    }
+
+    /**
+     *
+     */
+    public void printForStream() {
+        //stream()将集合转换成Stream
+        System.out.println(books.stream()
+                .filter(ele -> ((String) ele).contains("Java"))
+                .count());
+        System.out.println(books.stream()
+                .filter(ele -> ((String) ele).length() > 10)
+                .count());
+        //mapToInt()获取原有的Stream对应的IntStream
+        books.stream().mapToInt(ele -> ((String) ele).length())
+                .forEach(System.out::println);
+    }
+}
+
+/**
+ *
+ */
+class SplitNumAndLetter {
+    private String zifu = "ds123d4s4dqwAw57vv58gsTg578q93JNH21dsd2445";
+    private int[] num = new int[100];
+    private char[] letter = new char[100];
+    private int k = 0;
+    private int j = 0;
+
+    /**
+     * separate numbers and letters
+     */
+    public void mySplit() {
+        for (int i = 0; i < zifu.length(); i++) {
+            if (zifu.charAt(i) >= 'A' && zifu.charAt(i) <= 'z') {
+                letter[k++] = zifu.charAt(i);
+            } else {
+                num[j++] = zifu.charAt(i) - 48;
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            System.out.print(letter[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < j; i++) {
+            System.out.print(num[i]);
+        }
+//       System.out.println(Arrays.toString(letter));
+//       System.out.println(Arrays.toString(num));
+    }
+}
+
+/**
+ * Array
+ */
+class MyArray {
+    private int[] c = {1, 2, 3 };
+    private int[][] d = {{1, 2, 3, 4, 5 },
+            {1, 33, 8, 9, 7 }};
+    private String[][] str1 = new String[][]{new String[3], new String[]{"Hello"}};
+    private String[][] str2 = {new String[3], new String[]{"Hello"}};
+
+    /**
+     *
+     */
+    public void myprint() {
+        String[] strings1 = {"Hello"};
+        System.out.println(d[1][3]);
+        int[] arr1 = {3, -4, 25, 16, 30, 18};
+        //排序,利用了cpu并行提高性能
+        Arrays.parallelSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(arr1[1] == 3);
+        int[] arr2 = {3, 4, 25, 16, 30, 18};
+        //Arrays.parallelPrefix(arr2, (left,right)->left * right);
+        Arrays.parallelPrefix(arr2, new IntBinaryOperator() {
+            @Override
+            public int applyAsInt(int left, int right) {
+                return left * right;
+            }
+        });
+        System.out.println(Arrays.toString(arr2));
+        int[] arr3 = new int[5];
+        Arrays.parallelSetAll(arr3, new IntUnaryOperator() {
+            @Override
+            public int applyAsInt(int operand) {
+                return operand * 5;
+            }
+        });
+        System.out.println(Arrays.toString(arr3));
+    }
+
+}
+
--- src/test1.java
+++ src/test1.java
@@ -1,27 +1,172 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
-// 数组元素组合
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * Main
+ */
 public class test1 {
-//    public static void main(String[] args){
-//        String[] array = {"1","2","3","4"};
-////        List<String> strings = Arrays.asList(array);
-////        //strings.remove会报错
-////        List temp = new ArrayList(strings);
-////        System.out.println(temp.remove(1));//输出2
-//          listAll(Arrays.asList(array),"");
-//
-//    }
-    private static void listAll(List<String> strings, String s) {
-//        System.out.println(s);
-//        for(int i = 0; i <strings.size() ; i++){
-//            List temp = new ArrayList(strings);
-//            listAll(temp , s + temp.remove(i));
-//        }
+
+    private int test = 0;
+
+    public test1() {
+        System.out.println(test);
+    }
+
+    /**
+     * @param s The object of print
+     */
+    public static void myPrint(String s) {
+        System.out.println(s);
+    }
+
+    /**
+     *
+     * @param arg  parameter for main
+     */
+    public static void main(String[] arg) {
+
+//        new MyArray().myprint();
+//        ParentClass a = new ChildClass(4, 5, 6);
+//        ChildClass b = new ChildClass(1, 2, 3);
+//        ((ChildClass) a).childFunction(); //true
+//        a.childFunction(); //error
+//        b.parentFunction(); //true
+//         Object[] a= {1,2,3,5,4,8,6,7};
+//        perm b = new perm(a,1,8);
+//        System.out.println( a[0]);
+//        Statictest a = new Statictest(String.valueOf("1946"));
+//        System.out.println(a.isBornBoomer());
+        //System.out.println(String.valueOf("1946"));
     }
 }
 
-//https://blog.csdn.net/xiaoli56789/article/details/54907000
 
+/**
+ *The test of super
+ */
+class ParentClass {
+    private int i = 0;
+    private int j = 0;
+
+    /**
+     * null constructor
+     */
+//    ParentClass(){
+
+//    }
+    /**
+     *
+     * @param i test
+     * @param j test
+     */
+    ParentClass(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+    /**
+     * test
+     */
+    public void parentFunction() {
+        System.out.println(1);
+    }
+}
+
+/**
+ *
+ */
+class  ChildClass extends ParentClass {
+    private int k;
+    ChildClass(int i, int j, int k) {
+        //If the constructor of parent class has parameters(no empty constructor),
+        //the child class must be explicitly called with "super"
+        super(i, j);
+        this.k = k;
+    }
+
+    /**
+     * test
+     */
+    public void childFunction() {
+        System.out.println(2);
+    }
+}
+
+
+
+
+/**
+ * //排列问题
+ */
+class Perm {
+    private int a;
+
+    /**
+     *
+     * @param list test
+     * @param k test
+     * @param m test
+     */
+    public void  perm1(Object[] list, int k, int m) {
+        if (k == m) {
+            for (int i = 0; i <= m; i++) {
+                System.out.println(list[i]);
+            }
+            System.out.println();
+        } else {
+            for (int i = k; i <= m; i++) {
+                swap(list, k, i);
+                perm1(list, k + 1, m);
+                swap(list, k, i);
+            }
+        }
+
+    }
+
+    /**
+     *
+     * @param list test
+     * @param k test
+     * @param i test
+     */
+    public void swap(Object[] list, int k, int i) {
+        Object temp;
+        temp = list[k];
+        list[k] = list[i];
+        list[i] = temp;
+
+    }
+
+
+}
+
+/**
+ * static
+ */
+class StaticTest {
+    private String birthString;
+    private static String startString, endString;
+
+    static {
+
+        startString = String.valueOf("1946");
+        endString = String.valueOf("1964");
+    }
+
+    /**
+     * @param birthString test
+     */
+    StaticTest(String birthString) {
+        this.birthString = birthString;
+    }
+
+    /**
+     * @return test
+     */
+    boolean isBornBoomer() {
+        return birthString.compareTo(startString) >= 0 && birthString.compareTo(endString) < 0;
+    }
+}
